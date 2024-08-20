3da813a8a3da17e0371bc61bad0b17dbe84bf08f
Hieu <trunghieubui88@gmail.com>
2024-08-20 11:48:56 +0100

    add toolbox header text

--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -1,7 +1,15 @@
 // src/components/Toolbox.tsx
 
 import React from 'react';
-import { Box, SimpleGrid, Icon, Tooltip } from '@chakra-ui/react';
+import {
+  Box,
+  SimpleGrid,
+  Icon,
+  Tooltip,
+  VStack,
+  Divider,
+  Text,
+} from '@chakra-ui/react';
 import { Account } from '../items/Account';
 import { Instruction } from '../items/Instruction';
 import { Program } from '../items/Program';
@@ -15,24 +23,38 @@ const toolboxItems = [
 const Toolbox: React.FC = () => {
   return (
     <Box width='30%' maxWidth='200px' bg='gray.100' p={4}>
-      <SimpleGrid spacing={4} columns={3}>
-        {toolboxItems.map((item) => (
-          <Tooltip key={item.id} label={item.getName()} placement='right'>
-            <Box
-              as='button'
-              draggable
-              onDragStart={(e: any) =>
-                e.dataTransfer.setData('text/plain', item.getType())
-              }
-              p={2}
-              borderRadius='md'
-              _hover={{ bg: 'gray.200' }}
-            >
-              <Icon as={item.getIcon()} boxSize={6} />
-            </Box>
-          </Tooltip>
-        ))}
-      </SimpleGrid>
+      <VStack spacing={2} align='stretch'>
+        <Text fontWeight='light' textAlign='left'>
+          Drag items into canvas
+        </Text>
+        <Divider />
+        <SimpleGrid columns={2} spacing={4}>
+          {toolboxItems.map((item) => (
+            <Tooltip key={item.id} label={item.getName()} placement='right'>
+              <Box
+                as='button'
+                draggable
+                onDragStart={(e: any) =>
+                  e.dataTransfer.setData('text/plain', item.getType())
+                }
+                p={2}
+                borderRadius='md'
+                border='1px solid'
+                borderColor='gray.300'
+                _hover={{ bg: 'gray.200' }}
+                display='flex'
+                flexDirection='column'
+                alignItems='center'
+                justifyContent='center'
+                height='80px'
+              >
+                <Icon as={item.getIcon()} boxSize={6} mb={2} />
+                <Text fontSize='sm'>{item.getName()}</Text>
+              </Box>
+            </Tooltip>
+          ))}
+        </SimpleGrid>
+      </VStack>
     </Box>
   );
 };
